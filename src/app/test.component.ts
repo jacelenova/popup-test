8aba6b219cae8a0e46f7f8c42ad3916d23c31874
jacelenova <jacel.enova@yahoo.com>
2017-08-08 18:06:06 +0800

    changes

--- src/app/test.component.ts
+++ src/app/test.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'test-component',
